fc7fa9a03692bf67c73f3597a5070bdae29d3778
EvanM-Git <kixew34@gmail.com>
2025-04-01 15:07:22 -0500

    Save 0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n--- Calculadora Básica ---");
+    println!("1. Sumar");
+    println!("2. Restar");
+    println!("3. Multiplicar");
+    println!("4. Dividir");
+    println!("5. Potencia");
+    println!("6. Salir");
+
+    let mut numero1 = String::new();
+    let mut numero2 = String::new();
+    let mut opcion = String::new();
+    let mut resultado: f64 = 0.0;
+
+    loop {
+        println!("Selecciona una opción (1-5): ");
+        
+    }
 }
